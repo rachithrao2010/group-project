0747e33b56548276043144c2b044f25889253b74
rachithrao2010 <rachithrao2010@gmail.com>
2025-10-22 17:33:41 -0500

    added tests

--- problem1.py
+++ problem1.py
@@ -7,3 +7,8 @@ def reverse(string):
     new_string = separator.join(list)
     return new_string
 
+print(reverse("Hello"))
+print(reverse("Encyclopedia"))
+print(reverse("Table"))
+print(reverse("Wall-e"))
+print(reverse("speaker"))
