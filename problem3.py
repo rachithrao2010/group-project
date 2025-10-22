b84d78dc015f50173fc1b138222b4d30810a3692
Sushi72 <srinisus72@gmail.com>
2025-10-22 17:38:08 -0500

    Update problem3.py

---
+++ problem3.py
@@ -0,0 +1,13 @@
+# Find if number is even or not
+# Take number as an input
+# If yes, return True
+# If not, return false
+
+def is_even(number):
+    return number % 2 == 0
+
+print(is_even(4))
+print(is_even(92)) 
+print(is_even(7)) 
+print(is_even(0) 
+print(is_even(19823)) 
